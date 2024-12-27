eaeea12f485a809989ce6566f88ee7d633505958
Tomasgb97 <tomasgomezbelcastro@gmail.com>
2024-12-27 18:05:03 -0300

    feat(cart): Adds logic to remove objects and show price on cart.
    This changes add the logic to the cart page and comonents to remove items from the zustand store.
    Show the total price of the selected items.
    Makes some Ui changes to display an empty cart correctly and minor ui improves.

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,5 +1,5 @@
 'use client'
-import mockupData from '@/utils/mockData';
+
 import Link from 'next/link';
 import React from 'react';
 import Back from '../../../public/Icons/Back';
@@ -7,12 +7,12 @@ import Button from '../common/Button';
 import CartHeader from './CartHeader';
 import CartItem from './CartItem';
 import OrderSummary from './OrderSummary';
+import { useCartStore } from '@/services/stores/cart-store';
 
 const Cart: React.FC = () => {
-   
+   const {items, removeItem} = useCartStore()
         return (
             <>
-            
             <div className="w-full px-6 lg:px-32 py-4 lg:py-6 ">
             <Link className="w-fit flex justify-start items-center gap-1" href={'/'}>
             <Back/>
@@ -22,13 +22,16 @@ const Cart: React.FC = () => {
             <div className="w-full px-6 lg:px-32 py-8 lg:py-12 ">
                 <CartHeader/>
     
-                <div className=" w-full flex flex-col items-center lg:items-start lg:flex-row gap-12 lg:gap-20">
+                <div className=" w-full flex flex-col items-center lg:items-start lg:flex-row lg:justify-between gap-12 lg:gap-20">
                     <div className="flex flex-col divide-y divide-accent-gray">
-                        {mockupData.map((element, index)=>{
+                        {items.map((element, index)=>{
                             return(<div className="py-5 px-4 " key={index}>
-                                <CartItem description={element.description} genre={element.genre} imageUrl={element.image} name={element.name} price={element.price} key={element.id} id={element.id}/>
+                                <CartItem onRemove={()=>removeItem(element.id)} game={element}  key={element.id}/>
                             </div>)
                             })}
+
+                        {items.length == 0 && <div className='lg:min-w-96'>
+                            <h1>Empty cart</h1></div>}
     
                     </div>
                     
--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,38 +1,35 @@
 import React from 'react';
 import Close from '../../../public/Icons/Close';
+import { Game } from '@/utils/endpoint';
 
 interface CartItemProps {
-    id: number;
-    genre: string;
-    imageUrl: string;
-    name: string;
-    description: string;
-    price: number;
+    game: Game,
+    onRemove: ()=> void;
 
 }
 
-const CartItem: React.FC<CartItemProps> = ({ id, genre, name, description, price, imageUrl }) => {
+const CartItem: React.FC<CartItemProps> = ({ game, onRemove }) => {
     return (
         <div className='flex flex-col gap-11 lg:gap-0'>
-            <div className='flex gap-3'>
+            <div className='flex justify-between'>
             <div className='flex flex-col gap-4 md:flex-row'>
                 <div className='max-h-40 w-full'>
-                    <img className='w-full h-full object-cover' src={imageUrl} alt={`${name} video game image`} />
+                    <img className='w-full h-full object-cover' src={game.image} alt={`${game.name} video game image`} />
                 </div>
 
                 <div className='flex-col gap-3'>
-                    <h4 className='text-sm font-bold text-main-gray'>{genre}</h4>
-                    <h3 className='text-lg font-bold'>{name}</h3>
-                    <p className='text-md text-accent-gray w-full text-wrap'>{description}</p>
+                    <h4 className='text-sm font-bold text-main-gray'>{game.genre}</h4>
+                    <h3 className='text-lg font-bold'>{game.name}</h3>
+                    <p className='text-md text-accent-gray w-full text-wrap'>{game.description}</p>
 
                 </div>
             </div>
-            <div className='flex flex-col justify-start'>
+            <div onClick={onRemove} className='flex flex-col justify-start cursor-pointer'>
             <Close width={24} height={24}/>
             </div>
             
         </div>
-            <p className=' w-full text-right text-lg font-bold place-self-end'>{`$ ${price}`}</p>
+            <p className=' w-full text-right text-lg font-bold place-self-end'>{`$ ${game.price}`}</p>
         </div>
         
     );
